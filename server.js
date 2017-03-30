caf8c27ebe24b5c536dbcc567ba8d1ca43d6dd0d
anusone nanthavongsa <anusone nanthavongsa>
2017-03-30 16:51:17 -0400

    added express router and made route path

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
 
+
+// MIDDLEWARE
+var bodyParser = require('body-parser');
+
 //SET PORT
 app.set('port', 3000);
+// ROUTER
+var router = express.Router();
 
 //SERVER LISTENING
 var server = app.listen(app.get('port'), function(){
-  var port = server.address().port;
-  console.log('listening on port 3000');
+var port = server.address().port;
+console.log('listening on port 3000');
+});
+
+//ROUTE PREFIX
+app.use('/api', router);
+
+
+router.get('/', function(req, res){
+  res.json({message: "We did it"});
 });
